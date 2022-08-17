ed46814fa2006e3571d7ce1d6935f1c397cec77c
Rasulbek Qodirov <rasulbek1301@gmail.com>
2022-08-17 08:59:48 +0500

    first commit

--- base/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import StatistikaView, WorkerView
-
-urlpatterns = [
-    path('stat/', StatistikaView.as_view()),
-    path('worker/', WorkerView.as_view()),
-]
