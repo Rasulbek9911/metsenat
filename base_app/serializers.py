29a340e5f9f41edd94d4b258e7e209f98bf4ff3c
Rasulbek Qodirov <rasulbek1301@gmail.com>
2022-08-18 10:28:01 +0500

    Commit message3

--- base_app/serializers.py
+++ base_app/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from . import models
-from rest_framework.exceptions import ValidationError
 
 
 class SponsorSerializer(serializers.ModelSerializer):
@@ -18,26 +17,6 @@ class StudentSerializer(serializers.ModelSerializer):
 class ContractSerializer(serializers.ModelSerializer):
     student = StudentSerializer(read_only=True)
     sponsor = SponsorSerializer(read_only=True)
-
-    # def create(self, validated_data):
-    #     payment = validated_data['payment']
-    #
-    #     student = StudentSerializer(data=validated_data.pop('student'))
-    #     student_wallet = student.initial_data['wallet']
-    #     student_contract_summ = student.initial_data['contract_summ']
-    #
-    #     sponsor = SponsorSerializer(data=validated_data.pop('sponsor'))
-    #     sponsor_wallet = sponsor.initial_data['wallet']
-    #     if payment > sponsor_wallet:
-    #         raise ValidationError(detail='Sponsorning puli yetarlimas')
-    #     sponsor_wallet -= payment
-    #     if sponsor.is_valid():
-    #         sp = sponsor.save(user=self.context['request'].user)
-    #     if student.is_valid():
-    #         st = student.save(user=self.context['request'].user)
-    #     contract = models.Contract.objects.create(**validated_data, sponsor=sp, student=st,
-    #                                               user=self.context['request'].user)
-    #     return contract
 
     class Meta:
         model = models.Contract
